11490606a1d443e1b92e4b69fefe50c5f308c9a6
surnan <forsuresh@yandex.com>
2018-12-16 14:31:34 -0500

    + Anchor Extensions to minimize the typing needed to codify constraints
    Showing three color boxes as test for our stackView

--- TikTokSim/TikTokSim/ViewController.swift
+++ TikTokSim/TikTokSim/ViewController.swift
@@ -12,22 +12,29 @@ class ViewController: UIViewController {
 
     
     let stackView: UIStackView = {
-       let sv = UIStackView()
-        
+       let sv = UIStackView(arrangedSubviews: [])
         sv.translatesAutoresizingMaskIntoConstraints = false
         return sv
     }()
     
+    class iconView: UIView {
+        override var intrinsicContentSize: CGSize {
+            return .init(width: 80, height: 120)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
-        
+        let colors: [UIColor] = [.red, .green, .blue]
+        let subviews = colors.map { (color) -> UIView in
+            let v = iconView()
+            v.backgroundColor = color
+            return v
+        }
+        subviews.forEach({stackView.addArrangedSubview($0)})
+        view.addSubview(stackView)
+        stackView.anchor(top: nil, leading: nil, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
     }
-
-    
-
 }
 
